13fc7af03807ed0ff613e4183e18ee58afe43ef4
naomiraider <97920424+naomiraider@users.noreply.github.com>
2023-09-13 13:04:52 -0700

    jumbotron background, chloe font

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log ("connected")
+$("#proto1-btn").click(function () {
+  window.open(
+    "https://www.figma.com/proto/TxNZCYXiVp4zJib9lrJqL8/Hi5-Dog-Rescue-Redesign?page-id=0%3A1&type=design&node-id=1-3253&viewport=3853%2C3009%2C0.5&t=g4JBx1skm1S5tDWZ-1&scaling=scale-down&starting-point-node-id=1%3A3253&mode=design",
+    "_blank"
+  );
+});
